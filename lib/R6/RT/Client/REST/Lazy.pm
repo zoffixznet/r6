1c3728b3876a1d4f0e738f438ac6adb0793fe7e2
Zoffix Znet <cpan@zoffix.com>
2016-08-24 18:27:44 -0400

    Use long format

--- lib/R6/RT/Client/REST/Lazy.pm
+++ lib/R6/RT/Client/REST/Lazy.pm
@@ -3,6 +3,7 @@ use Mew;
 use Mojo::Util qw/trim/;
 use Mojo::UserAgent;
 use URI::Escape;
+use 5.024;
 
 has [qw/_login  _pass/] => Str;
 has _server => Str, default => 'https://rt.perl.org/REST/1.0';
@@ -23,6 +24,7 @@ sub search {
     my ($self, %opts) = @_;
     $opts{not_status} = ['resolved', 'rejected']
         unless $opts{status} or $opts{not_status};
+    $opts{format} //= 'l';
 
     my $cond = join " AND ",
         ($opts{after}  ? "Created >= '$opts{after}'"  : () ),
@@ -40,25 +42,44 @@ sub search {
 
     my $url = $self->_server . '/search/ticket?' . $self->_lp
         . '&orderby=-Created'
+        . '&format=' . $opts{format}
         . '&query=' . uri_escape("Queue = 'perl6' AND ($cond)");
 
     my $tx = $self->_ua->get($url);
+    use Acme::Dump::And::Dumper;
+        die DnD [ $url, $tx,  $tx->success, $tx->res->code, $tx->res->body, $tx->res->headers->to_string ];
     return unless $tx->success;
     my $c = $tx->res->body;
+    use Acme::Dump::And::Dumper;
+    die DnD [ $c ];
     return unless $c =~ s{^RT/[\d.]+ 200 Ok\s+}{};
     $c = trim $c;
-
     my @tickets;
-    for ( split /\r?\n\r?/, $c ) {
-        next unless /^\d+:.+/;
-        my ( $id, $subject ) = split /: /, $_, 2;
-        my @tags = $subject =~ /(?<=\[) [\@A-Z]+ (?=\])/gx;
-        push @tickets, {
-            id      => $id,
-            subject => $subject,
-            tags    => \@tags,
-        };
+    if ( $opts{format} eq 's' ) {
+        for ( split /\r?\n\r?/, $c ) {
+            next unless /^\d+:.+/;
+            my ( $id, $subject ) = split /: /, $_, 2;
+            my @tags = $subject =~ /(?<=\[) [\@A-Z]+ (?=\])/gx;
+            push @tickets, {
+                id      => $id,
+                subject => $subject,
+                tags    => \@tags,
+            };
+        }
     }
+    elsif ( $opts{format} eq 'l') {
+        for ( split /\n\n--\n\n/xm, $c ) {
+            my %ticket;
+            for ( split /\n/ ) {
+                my ($key, $value) = split /:\s+/, $_, 2;
+                $ticket{$key} = $value // '';
+            }
+            ( $ticket{id} ) = $ticket{id} =~ /\d+/g;
+            push @tickets, \%ticket;
+        }
+    }
+    use Acme::Dump::And::Dumper;
+    print DnD [ @tickets ];
     return @tickets;
 }
 
