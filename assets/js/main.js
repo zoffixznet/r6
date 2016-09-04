329b7fa32eccbd2e90c2b6b7946a4f3e970502e5
Zoffix Znet <cpan@zoffix.com>
2016-09-04 07:07:47 -0400

    Make ticket review buttons ajaxy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(function(){
     setup_commit_review_links();
+    setup_ticket_review_links();
 });
 
 function setup_commit_review_links() {
@@ -17,6 +18,32 @@ function setup_commit_review_links() {
                                 .toggleClass('glyphicon-question-sign')
                     .parents('li')
                         .toggleClass('a');
+            }
+        });
+    });
+}
+
+function setup_ticket_review_links() {
+    $('#tickets-list .ac a.btn').click(function (e) {
+        var el = $(this);
+        e.preventDefault();
+
+        $.ajax({
+            url: el.attr('href'),
+            error: function() { alert("Request failed") },
+            success: function() {
+                if ( el.hasClass('reviewed') ) {
+                    el  .toggleClass('btn-default')
+                        .toggleClass('btn-info')
+                        .parents('tr')
+                            .toggleClass('r');
+                }
+                else if ( el.hasClass('blocker') ) {
+                    el  .toggleClass('btn-default')
+                        .toggleClass('btn-danger')
+                        .parents('tr')
+                            .toggleClass('b');
+                }
             }
         });
     });
